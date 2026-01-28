61b666ed8a11234289a98a773a578cd6ce00db8a
McLareni <mukola.kosobutski05@gmail.com>
2026-01-28 13:25:38 +0100

    Fix Prisma client import and add generate to build script

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,18 +1,19 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: InstanceType<typeof PrismaClient> | undefined;
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
 };
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = process.env.DATABASE_URL || "";
 
 const adapter = new PrismaPg({ connectionString });
-const prismaClient = new PrismaClient({ adapter });
 
-export const prisma =
-  globalForPrisma.prisma ??
-  (prismaClient as InstanceType<typeof PrismaClient>);
+const prismaClient = new PrismaClient({
+  adapter,
+});
+
+export const prisma = globalForPrisma.prisma ?? prismaClient;
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
